b9543f64bd6a13d2a678465856cade57376b10a0
Tez Park <tez.park@sendbird.com>
2025-03-20 16:05:47 +0900

    Release - 0.0.1

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# sendbird-ai-agent-core-ios
+# SendbirdAIAgentCore
+
+## Overview
+
+This repository contains the core package for the Sendbird AI Agent. It is designed to be used as a Swift Package Manager (SPM) dependency in other projects.
+
+## Usage
+
+This package is not intended for direct use by customers. It serves as a core component for other Sendbird AI Agent projects.
---
+++ SendbirdAIAgentCore/SendbirdAIAgentCore.h
@@ -0,0 +1,18 @@
+//
+//  SendbirdAIAgentCore.h
+//  SendbirdAIAgentCore
+//
+//  Created by Tez Park on 3/19/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SendbirdAIAgentCore.
+FOUNDATION_EXPORT double SendbirdAIAgentCoreVersionNumber;
+
+//! Project version string for SendbirdAIAgentCore.
+FOUNDATION_EXPORT const unsigned char SendbirdAIAgentCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SendbirdAIAgentCore/PublicHeader.h>
+
+
