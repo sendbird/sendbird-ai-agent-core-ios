f77b00f9027c8f3261f4856b80559d302a38f1df
Tez Park <tez.park@sendbird.com>
2025-03-21 02:29:43 +0900

    [MOD] modified Package dependencies

--- Package.swift
+++ Package.swift
@@ -7,12 +7,43 @@ let package = Package(
     products: [
         .library(name: "SendbirdAIAgentCore", targets: ["SendbirdAIAgentCore"])
     ],
-    dependencies: [],
+    dependencies: [
+        .package(
+            name: "SendbirdChatSDK",
+            url: "https://github.com/sendbird/sendbird-chat-sdk-ios",
+            from: "4.25.2-beta"
+        ),
+        .package(
+            name: "MarkdownUI",
+            url: "https://github.com/gonzalezreal/swift-markdown-ui",
+            from: "2.4.1"
+        ),
+        .package(
+            url: "https://github.com/JohnSundell/Splash",
+            from: "0.16.0"
+        ),
+        .package(
+            name: "SendbirdUIMessageTemplate",
+            url: "https://github.com/sendbird/sendbird-uikit-ios",
+            from: "3.30.0"
+        )
+    ],
     targets: [
         .binaryTarget(
             name: "SendbirdAIAgentCore",
             url: "https://github.com/sendbird/sendbird-ai-agent-core-ios/releases/download/0.0.1/SendbirdAIAgentCore.xcframework.zip",
             checksum: "e2c833130d9acc03df44ba703556cd2d6f21d615a98e43dc14e20a883ccc4e6c"
+        ),
+        .target(
+            name: "SendbirdAIAgentCoreTarget",
+            dependencies: [
+                .target(name: "SendbirdAIAgentCore"),
+                .product(name: "SendbirdChatSDK", package: "SendbirdChatSDK"),
+                .product(name: "SendbirdUIMessageTemplate", package: "SendbirdUIMessageTemplate"),
+                .product(name: "MarkdownUI", package: "MarkdownUI"),
+                .product(name: "Splash", package: "Splash")
+            ],
+            path: "Framework/Dependency"
         )
     ]
 )
