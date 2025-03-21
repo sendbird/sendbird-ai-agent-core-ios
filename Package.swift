334ea840f9061b3260254856b7a5a4cdd50faa4f
Tez Park <tez.park@sendbird.com>
2025-03-22 00:20:17 +0900

    [MOD] modified version

--- Package.swift
+++ Package.swift
@@ -31,7 +31,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SendbirdAIAgentCore",
-            url: "https://github.com/sendbird/sendbird-ai-agent-core-ios/releases/download/0.0.2/SendbirdAIAgentCore.xcframework.zip",
+            url: "https://github.com/sendbird/sendbird-ai-agent-core-ios/releases/download/0.0.1/SendbirdAIAgentCore.xcframework.zip",
             checksum: "e2c833130d9acc03df44ba703556cd2d6f21d615a98e43dc14e20a883ccc4e6c"
         ),
         .target(
