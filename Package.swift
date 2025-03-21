05b727505d3015884b844caaceb95ea67d1ac0db
Tez Park <tez.park@sendbird.com>
2025-03-21 23:40:28 +0900

    Updated version

--- Package.swift
+++ Package.swift
@@ -24,14 +24,14 @@ let package = Package(
         ),
         .package(
             name: "SendbirdUIMessageTemplate",
-            url: "https://github.com/sendbird/sendbird-uikit-ios",
-            from: "3.30.0"
+            url: "https://github.com/sendbird/sendbird-uikit-ios-spm",
+            from: "3.30.1"
         )
     ],
     targets: [
         .binaryTarget(
             name: "SendbirdAIAgentCore",
-            url: "https://github.com/sendbird/sendbird-ai-agent-core-ios/releases/download/0.0.1/SendbirdAIAgentCore.xcframework.zip",
+            url: "https://github.com/sendbird/sendbird-ai-agent-core-ios/releases/download/0.0.2/SendbirdAIAgentCore.xcframework.zip",
             checksum: "e2c833130d9acc03df44ba703556cd2d6f21d615a98e43dc14e20a883ccc4e6c"
         ),
         .target(
